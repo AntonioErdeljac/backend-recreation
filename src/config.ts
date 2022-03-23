6faf008baefc378591127016056fcc0c22f97003
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2022-03-23 18:02:24 +0100

    feat: add config

---
+++ src/config.ts
@@ -0,0 +1,11 @@
+const PORT = process.env.PORT || 8080;
+const DOMAIN = process.env.DOMAIN || 'localhost';
+const PROTOCOL = process.env.PROTOCOL || 'http://';
+const CLIENT_URL = 'http://localhost:8080';
+
+export default {
+  PORT,
+  DOMAIN,
+  PROTOCOL,
+  CLIENT_URL,
+};
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import http from 'http';
 
+import config from './config';
+
 const app = express();
 const server = http.createServer(app);
 
-server.listen(8000, () => {
-  console.log('Server running at http://localhost:8000/');
+server.listen(config.PORT, () => {
+  console.log(`Server running at ${config.CLIENT_URL}`);
 });
