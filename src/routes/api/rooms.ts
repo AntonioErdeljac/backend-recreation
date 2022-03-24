ea74e3d56ad76067849ca3adb2ffb665b2ebd88f
Antonio Erdeljac <erdeljac.antonio@gmail.com>
2022-03-24 02:20:52 +0100

    feat: add check-session middleware to rooms api

--- src/routes/api/rooms.ts
+++ src/routes/api/rooms.ts
@@ -2,7 +2,8 @@ import * as express from 'express';
 
 import { rooms } from '../../controllers';
 import { paths } from '../../constants';
+import { checkSession } from '../../middlewares';
 
 export default (router: express.Router): void => {
-  router.post(paths.ROOMS, rooms.create);
+  router.post(paths.ROOMS, checkSession(), rooms.create);
 };
